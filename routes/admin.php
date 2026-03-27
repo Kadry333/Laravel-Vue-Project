247fdabd456162e201dcd01086a99ef7d6c16449
Amr Shoukry <shoukryworkamr1@gmail.com>
2026-03-27 16:30:59 +0200

    Added managers

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminDashboard\ManagerController;
 use App\Http\Controllers\AdminDashboard\RoomController;
 
 use Illuminate\Support\Facades\Route;
@@ -15,3 +16,15 @@ Route::prefix('rooms')->as('rooms.')->group(function () {
     Route::post('/{room}',       [RoomController::class, 'update'])->name('update');
     Route::delete('/{room}',     [RoomController::class, 'destroy'])->name('destroy');
 });
+
+// ======================== Managers ====================
+Route::prefix('managers')->as('managers.')->group(function () {
+    Route::get('/',                 [ManagerController::class, 'index'])->name('index');
+    Route::get('/create',           [ManagerController::class, 'create'])->name('create');
+    Route::post('/',                [ManagerController::class, 'store'])->name('store');
+    Route::get('/{manager}',        [ManagerController::class, 'show'])->name('show');
+    Route::get('/{manager}/edit',   [ManagerController::class, 'edit'])->name('edit');
+    Route::post('/{manager}',       [ManagerController::class, 'update'])->name('update');
+    Route::delete('/{manager}',     [ManagerController::class, 'destroy'])->name('destroy');
+});
+
