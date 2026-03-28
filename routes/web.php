2b7ff35dacac5ff5a6b5871a33ab11a86eeb5fa1
mawadaelmeshad <mawadahassan21@gmail.com>
2026-03-28 22:05:19 +0200

    added client table, toggle approav button

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    'canLogin' => Route::has('login'),
+    'canRegister' => Route::has('register'),
+    'laravelVersion' => Application::VERSION,
+    'phpVersion' => PHP_VERSION,
     ]);
 });
 
@@ -21,9 +21,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'logs-out-banned-user', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'logs-out-banned-user'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class , 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class , 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class , 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
@@ -44,4 +44,3 @@ Route::middleware(['auth', 'logs-out-banned-user', 'role:receptionist'])->get('/
 Route::middleware(['auth', 'logs-out-banned-user', 'role:client'])->get('/client', function () {
     return Inertia::render('RolePage', ['role' => 'Client']);
 });
-
